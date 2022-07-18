d9bc7ef52d39622ccbb29bdc14aad7d4347c0560
John Parkhurst <jparkhurst120@gmail.com>
2022-07-18 16:15:16 -0400

    Update main.go

--- src/statistics/main.go
+++ src/statistics/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	fmt.Println(apiKey)
+	var url = "https://aerodatabox.p.rapidapi.com/flights/%7BsearchBy%7D/KL1395/2020-06-10"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("X-RapidAPI-Key", "SIGN-UP-FOR-KEY")
+	req.Header.Add("X-RapidAPI-Host", "aerodatabox.p.rapidapi.com")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
 }
