62b77a370f6f213243c5fe4adba0c8366e39783c
John Parkhurst <jparkhurst120@gmail.com>
2022-07-27 00:21:47 -0400

    perkStyle/stats

--- src/statistics/matchDS.go
+++ src/statistics/matchDS.go
@@ -35,3 +35,26 @@ type participantDTO struct {
 }
 type teamDTO struct {
 }
+type perksDTO struct {
+	statPerks perkStatsDTO
+	styles    []perkStyleDTO
+}
+
+type perkStatsDTO struct {
+	defense int
+	flex    int
+	offense int
+}
+
+type perkStyleDTO struct {
+	description string
+	selections  []perkStyleSelectionDTO
+	style       int
+}
+
+type perkStyleSelectionDTO struct {
+	perk int
+	var1 int
+	var2 int
+	var3 int
+}
