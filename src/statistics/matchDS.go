aabc062ae4c3f44210f5287375f1df2b7d9e1da3
John Parkhurst <jparkhurst120@gmail.com>
2022-07-27 00:14:12 -0400

    infoDS

--- src/statistics/matchDS.go
+++ src/statistics/matchDS.go
@@ -8,10 +8,30 @@ type matchDTO struct {
 }
 
 type infoDTO struct {
+	gameCreation       int64
+	gameDuration       int64
+	gameEndTimestamp   int64
+	gameId             int64
+	gameMode           string
+	gameName           string
+	gameStartTimestamp int64
+	gameType           string
+	gameVersion        string
+	mapId              int
+	participants       []participantDTO
+	platformId         string
+	queueId            int
+	teams              []teamDTO
+	tournamentCode     string
 }
 
 type metadataDTO struct {
 	dataVersion  string
 	matchId      string
 	participants []string
+}
+
+type participantDTO struct {
+}
+type teamDTO struct {
 }
