8ea6f79a55a652eb33b0be32aaf34b3fd736a04e
John Parkhurst <jparkhurst120@gmail.com>
2022-08-03 02:44:50 -0400

    test

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -3,6 +3,20 @@
     <a class="active" href="#home">Home</a>
     <input type="text" placeholder="Search..">
   </div>
+
+  <p>Here is the contest</p>
+  <table>
+    <td>
+      <tr>row1</tr>
+      <tr>row2</tr>
+      <tr>row3</tr>
+    </td>
+    <td>
+      <tr>row1</tr>
+      <tr>row2</tr>
+      <tr>row3</tr>
+    </td>
+  </table>
 </template>
 
 <script>
--- src/statistics/config.go
+++ src/statistics/config.go
@@ -1,4 +1,6 @@
 package main
 
 //Just a file to save variables
-var apiKey = ""
+var apiKey = "RGAPI-727acb53-823c-4bc3-b352-6877390e103f"
+var testKey = "2c32c52680mshfce85c9bcc59db4p1a6f6cjsn097da7ccb84f"
+var fleetId = "axq1d9E4EORIU5GUVfmhwKBbO-NZqZfVOrWs3125ug686Utyt3wkr14G9-tIN0pUJQXdf3X-j_QLlw"
